56a337dc6a3e76476f5f4528979d78431e041b3b
kankan <yunshangbi-an@hotmail.com>
2025-05-31 09:49:57 +0800

    updated

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"  || exit /b 1
 
 echo 正在安装依赖...
-call npm install --registry https://registry.npmirror.com || goto error
+call npm install --registry https://registry.npmmirror.com || goto error
 
 echo 安装成功！按任意键退出
 pause >nul
