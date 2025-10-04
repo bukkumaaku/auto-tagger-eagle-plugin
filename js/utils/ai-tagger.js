14b5de282d363e893e69168f7c4e20ef8a21bc91
kankan <yunshangbi-an@hotmail.com>
2025-10-04 17:41:36 +0800

    支持cl_tagger模型，支持视频内容标签

--- js/utils/ai-tagger.js
+++ js/utils/ai-tagger.js
@@ -3,23 +3,41 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const sharp = require("sharp");
 const path = require("path");
+const ffmpeg = require("fluent-ffmpeg");
+const video_ext = ["mp4", "avi", "mov", "mkv", "flv", "wmv"];
+
+let wd_or_cl = "wd";
 
 async function getCsvData(filePath) {
 	const result = [];
-	const csvData = await new Promise((resolve, reject) => {
-		fs.createReadStream(filePath)
-			.pipe(csv())
-			.on("data", (data) => {
-				result.push(data);
-			})
-			.on("end", () => {
-				resolve(result);
-			})
-			.on("error", (error) => {
-				reject(error);
-			});
-	});
-	return csvData;
+	if (fs.existsSync(path.join(filePath, "selected_tags.csv"), fs.constants.R_OK)) {
+		wd_or_cl = "wd";
+		filePath = path.join(filePath, "selected_tags.csv");
+		const csvData = await new Promise((resolve, reject) => {
+			fs.createReadStream(filePath)
+				.pipe(csv())
+				.on("data", (data) => {
+					result.push(data);
+				})
+				.on("end", () => {
+					resolve(result);
+				})
+				.on("error", (error) => {
+					reject(error);
+				});
+		});
+		return csvData;
+	} else if (fs.existsSync(path.join(filePath, "tag_mapping.json"), fs.constants.R_OK)) {
+		wd_or_cl = "cl";
+		filePath = path.join(filePath, "tag_mapping.json");
+
+		const config = require(filePath);
+		const len = Object.keys(config).length;
+		for (let i = 0; i < len; i++) {
+			result.push({ name: config[i].tag });
+		}
+	}
+	return result;
 }
 async function createSession(modelPath) {
 	const executionProviders = [];
@@ -44,31 +62,81 @@ async function createSession(modelPath) {
 	});
 }
 
-const preprocessImage = async (imagePath) => {
-	let image = sharp(imagePath);
-	try {
-		image = image.flatten();
-		image = image.resize({
-			width: sizeSetting,
-			height: sizeSetting,
-			fit: "contain",
-			background: { r: 0, g: 0, b: 0 },
-		});
-		const channels = 3;
-		const imageData = await image.raw().toBuffer();
-		const reshapedData = new Uint8Array(sizeSetting * sizeSetting * channels);
-		for (let i = 0; i < imageData.length; i += 3) {
-			for (let channel = 0; channel < 3; channel++) {
-				reshapedData[i + 2 - channel] = imageData[i + channel] || 0;
-			}
+function postProcessLogits(logitsData) {
+	const CLIP_MIN = -30.0;
+	const CLIP_MAX = 30.0;
+	const probabilities = logitsData.map((logit) => {
+		let value = logit;
+		if (isNaN(value)) {
+			value = 0.0;
+		} else if (value === Infinity) {
+			value = 1.0;
+		} else if (value === -Infinity) {
+			value = 0.0;
 		}
-		const inputData = Float32Array.from(reshapedData);
-		return inputData;
+		const clippedValue = Math.max(CLIP_MIN, Math.min(CLIP_MAX, value));
+		return 1 / (1 + Math.exp(-clippedValue));
+	});
+	return Float32Array.from(probabilities);
+}
+
+async function preprocessImage(imagePath) {
+	try {
+		let image = sharp(imagePath);
+		if (wd_or_cl === "cl") {
+			image = image.flatten({ background: "#000000" });
+			image = image.resize({
+				width: sizeSetting,
+				height: sizeSetting,
+				fit: "contain",
+				background: { r: 0, g: 0, b: 0 },
+			});
+			const rawPixelData = await image.raw().toBuffer();
+			const channels = 3;
+			const inputData = new Float32Array(sizeSetting * sizeSetting * channels);
+
+			const mean = 0.5;
+			const std = 0.5;
+			for (let y = 0; y < sizeSetting; y++) {
+				for (let x = 0; x < sizeSetting; x++) {
+					const i = (y * sizeSetting + x) * channels;
+					const r = rawPixelData[i];
+					const g = rawPixelData[i + 1];
+					const b = rawPixelData[i + 2];
+					const normalized_b = (b / 255.0 - mean) / std;
+					const normalized_g = (g / 255.0 - mean) / std;
+					const normalized_r = (r / 255.0 - mean) / std;
+					inputData[y * sizeSetting + x] = normalized_b;
+					inputData[sizeSetting * sizeSetting + y * sizeSetting + x] = normalized_g;
+					inputData[2 * sizeSetting * sizeSetting + y * sizeSetting + x] = normalized_r;
+				}
+			}
+
+			return inputData;
+		} else {
+			image = image.flatten();
+			image = image.resize({
+				width: sizeSetting,
+				height: sizeSetting,
+				fit: "contain",
+				background: { r: 0, g: 0, b: 0 },
+			});
+			const channels = 3;
+			const imageData = await image.raw().toBuffer();
+			const reshapedData = new Uint8Array(sizeSetting * sizeSetting * channels);
+			for (let i = 0; i < imageData.length; i += 3) {
+				for (let channel = 0; channel < 3; channel++) {
+					reshapedData[i + 2 - channel] = imageData[i + channel] || 0;
+				}
+			}
+			const inputData = Float32Array.from(reshapedData);
+			return inputData;
+		}
 	} catch (error) {
-		console.log("无法读取图像", imagePath);
+		console.error("无法处理图像:", imagePath, error);
 		return [];
 	}
-};
+}
 async function runInference(imagePath) {
 	const results = [];
 	for (let i = 0; i < imagePath.length; i++) {
@@ -77,12 +145,56 @@ async function runInference(imagePath) {
 			results.push([]);
 			continue;
 		}
-		const tensor = new ort.Tensor("float32", floatData, [1, sizeSetting, sizeSetting, 3]);
+		let arr = [1, sizeSetting, sizeSetting, 3];
+		if (wd_or_cl === "cl") {
+			arr = [1, 3, sizeSetting, sizeSetting];
+		}
+		const tensor = new ort.Tensor("float32", floatData, arr);
 		results.push(tensor);
 	}
 	return results;
 }
-async function getTag(imagePath, session, tagSet, threshold) {
+
+function getVideoDuration(videoPath) {
+	return new Promise((resolve, reject) => {
+		ffmpeg.ffprobe(videoPath, (err, metadata) => {
+			if (err) {
+				return reject(err);
+			}
+			const duration = metadata.format.duration;
+			if (duration) {
+				resolve(duration);
+			} else {
+				reject(new Error("无法获取视频时长"));
+			}
+		});
+	});
+}
+
+function takeScreenshots(videoPath, outputFolder, timestamps) {
+	return new Promise((resolve, reject) => {
+		const outputFilenames = [];
+		const command = ffmpeg(videoPath)
+			.on("filenames", function (filenames) {
+				console.log("成功生成截图: " + filenames.join(", "));
+				filenames.forEach((name) => outputFilenames.push(path.join(outputFolder, name)));
+			})
+			.on("end", function () {
+				resolve(outputFilenames);
+			})
+			.on("error", function (err) {
+				console.error("截图过程中发生错误: " + err.message);
+				reject(err);
+			})
+			.screenshots({
+				timemarks: timestamps,
+				filename: "video.png",
+				folder: outputFolder,
+			});
+	});
+}
+
+async function getTag(imagePath, session, tagSet, config, imageItemsBatchs, isRecursion = false) {
 	const results = await runInference(imagePath);
 	let resultsLength = results.length;
 	if (results[0]?.data === undefined) return [];
@@ -103,22 +215,71 @@ async function getTag(imagePath, session, tagSet, threshold) {
 		offset += tensor.data.length;
 	}
 	// 运行模型
-	const batchTensor = new ort.Tensor("float32", concatenatedData, [imagePath.length, sizeSetting, sizeSetting, 3]);
+	let arr = [imagePath.length, sizeSetting, sizeSetting, 3];
+	if (wd_or_cl === "cl") {
+		arr = [imagePath.length, 3, sizeSetting, sizeSetting];
+	}
+	const batchTensor = new ort.Tensor("float32", concatenatedData, arr);
 	const feeds = {};
 	if (session.inputNames[0]) {
 		feeds[session.inputNames[0]] = batchTensor;
 	}
 	const result = await session.run(feeds);
 	if (result === undefined) return [];
-	const cpuData = result.output?.data || result.output?.cpuData || result.predictions_sigmoid?.cpuData;
+	let cpuData =
+		result.output?.data || result.output?.cpuData || result.predictions_sigmoid?.cpuData || result.output0.cpuData;
+	if (wd_or_cl === "cl") {
+		for (let i = 0; i < cpuData.length; i++) {
+			let value = cpuData[i];
+			if (isNaN(value)) {
+				value = 0.0;
+			} else if (value === Infinity) {
+				value = 1.0;
+			} else if (value === -Infinity) {
+				value = 0.0;
+			}
+			const clippedValue = Math.max(-30, Math.min(value, 30));
+			const prob = 1 / (1 + Math.exp(-clippedValue));
+			cpuData[i] = prob;
+		}
+	}
 	const perBatchSize = cpuData.length / imagePath.length;
 	let ans = [];
 	for (let i = 0; i < imagePath.length; i++) {
 		let tag = [];
 		for (let j = 0; j < perBatchSize; j++) {
-			if (cpuData[i * perBatchSize + j] > threshold) {
+			if (cpuData[i * perBatchSize + j] > config.threshold) {
 				tag.push(tagSet[j].name);
 			}
+		}
+		if (
+			video_ext.includes(imageItemsBatchs[i]?.ext.toLowerCase()) &&
+			(await eagle.extraModule.ffmpeg.isInstalled()) &&
+			!isRecursion &&
+			config.readVideo === "read"
+		) {
+			console.log("正在截取视频: " + imageItemsBatchs[i].filePath);
+			const inputFile = imageItemsBatchs[i].filePath;
+			const ffmpegPaths = await eagle.extraModule.ffmpeg.getPaths();
+			const ffmpegBinaryPath = ffmpegPaths.ffmpeg;
+			const ffprobeBinaryPath = ffmpegPaths.ffprobe;
+			ffmpeg.setFfmpegPath(ffmpegBinaryPath);
+			ffmpeg.setFfprobePath(ffprobeBinaryPath);
+			const duration = await getVideoDuration(inputFile);
+			const percentages = [1, 20, 40, 60, 80, 99];
+			const timestamps = percentages.map((percent) => {
+				return (duration * percent) / 100;
+			});
+			for (let i in timestamps) {
+				const savedFiles = await takeScreenshots(inputFile, __dirname, [i]);
+				const tag1 = await getTag(savedFiles, session, tagSet, config, [imageItemsBatchs[i]], true);
+				tag1[0].forEach((element) => {
+					if (!tag.includes(element)) {
+						tag.push(element);
+					}
+				});
+			}
+			fs.unlinkSync(path.join(__dirname, "video.png"));
 		}
 		ans.push(tag);
 	}
@@ -132,7 +293,7 @@ module.exports = async function aiTagger(images, imageItems, setTag, config) {
 	// 计算时间
 	console.time("aiTagger");
 	const session = await createSession(path.join(__dirname, "..", "..", "models", config.modelPath, "model.onnx"));
-	const tagSet = await getCsvData(path.join(__dirname, "..", "..", "models", config.modelPath, "selected_tags.csv"));
+	const tagSet = await getCsvData(path.join(__dirname, "..", "..", "models", config.modelPath));
 	let step = config.steps;
 	let imageBatchs = [];
 	let imageItemsBatchs = [];
@@ -148,7 +309,7 @@ module.exports = async function aiTagger(images, imageItems, setTag, config) {
 			continue;
 		}
 		if (step == countStep) {
-			const tag = await getTag(imageBatchs, session, tagSet, config.threshold);
+			const tag = await getTag(imageBatchs, session, tagSet, config, imageItemsBatchs);
 			await setTag(imageItemsBatchs, tag);
 			countStep = 0;
 			imageBatchs = [];
@@ -156,7 +317,7 @@ module.exports = async function aiTagger(images, imageItems, setTag, config) {
 		}
 	}
 	if (countStep > 0) {
-		const tag = await getTag(imageBatchs, session, tagSet, config.threshold);
+		const tag = await getTag(imageBatchs, session, tagSet, config, imageItemsBatchs);
 		await setTag(imageItemsBatchs, tag);
 		countStep = 0;
 		imageBatchs = [];
